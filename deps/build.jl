700d7a2fe7c11340dfec2be24fe0e7d54ac00b2d
Daniel Schwabeneder <daniel.schwabeneder@openmailbox.org>
2017-08-11 10:10:03 +0200

    allow download on windows

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,18 @@
+# download from https://github.com/JuliaLang/julia/pull/21968
+if is_windows()
+    const downloadcmd = :powershell
+    function download(url::AbstractString, filename::AbstractString)
+        ps = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
+        tls12 = "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12"
+        client = "New-Object System.Net.Webclient"
+        # in the following we escape ' with '' (see https://ss64.com/ps/syntax-esc.html)
+        downloadfile = "($client).DownloadFile('$(replace(url, "'", "''"))', '$(replace(filename, "'", "''"))')"
+        run(`$ps -NoProfile -Command "$tls12; $downloadfile"`)
+        filename
+    end
+end
+
+
 cd(dirname(@__FILE__)) do
     download("https://api.plot.ly/v2/plot-schema?sha1", "plotschema.json")
     download("https://cdn.plot.ly/plotly-latest.min.js", "plotly-latest.min.js")
